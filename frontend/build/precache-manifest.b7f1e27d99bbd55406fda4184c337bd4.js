e9ff73bae84dde6b826f341da983d1bc0555c027
Tommi Brusila <tommi.brusila@tuni.fi>
2020-02-06 12:38:39 +0200

    Add frontend build to git for deploying

---
+++ frontend/build/precache-manifest.b7f1e27d99bbd55406fda4184c337bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4e8c91fb287173dbda2793be44da2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5cc6aef42881f767c68",
+    "url": "/static/css/main.adfa3945.chunk.css"
+  },
+  {
+    "revision": "8b8c1f29f99d3bf30727",
+    "url": "/static/js/2.59096d3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.59096d3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5cc6aef42881f767c68",
+    "url": "/static/js/main.aff2bdb0.chunk.js"
+  },
+  {
+    "revision": "34c2a12524554cc35105",
+    "url": "/static/js/runtime-main.62ac5bc9.js"
+  }
+]);
